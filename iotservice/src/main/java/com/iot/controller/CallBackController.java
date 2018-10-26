fb6866203d02883f6d623aad71bb34081f146820
17734576784 <17734576784@163.com>
2018-10-26 16:41:19 +0800

    去掉无用引用

--- iotservice/src/main/java/com/iot/controller/CallBackController.java
+++ iotservice/src/main/java/com/iot/controller/CallBackController.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
