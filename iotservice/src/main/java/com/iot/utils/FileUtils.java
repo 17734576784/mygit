f0bc2ee61826c0ed95b550d7fbe6bd15ee026636
17734576784 <17734576784@163.com>
2018-11-08 17:00:36 +0800

    文件分解成字符数组

--- iotservice/src/main/java/com/iot/utils/FileUtils.java
+++ iotservice/src/main/java/com/iot/utils/FileUtils.java
@@ -4,10 +4,18 @@
 package com.iot.utils;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.client.ClientProtocolException;
@@ -17,6 +25,7 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.util.FileCopyUtils;
 
 import com.alibaba.fastjson.JSONObject;
 import com.iot.logger.LogName;
@@ -92,4 +101,135 @@ public class FileUtils {
 
 		return flag;
 	}
+	
+	/**
+	 * 文件转为二进制数组
+	 * @param file
+	 * @return
+	 */
+    public static byte[] fileToBinArray(File file){
+        try {
+            InputStream fis = new FileInputStream(file);
+            byte[] bytes = FileCopyUtils.copyToByteArray(fis);
+            return bytes;
+        }catch (Exception ex){
+            throw new RuntimeException("transform file into bin Array 出错",ex);
+        }
+    }
+
+	/**
+	 * 文件转为二进制字符串
+	 * @param file
+	 * @return
+	 */
+    public static String fileToBinStr(File file){
+        try {
+            InputStream fis = new FileInputStream(file);
+            byte[] bytes = FileCopyUtils.copyToByteArray(fis);
+            System.out.println(bytes);
+            System.out.println(bytes.length);
+            return new String(bytes,"ISO-8859-1");
+        }catch (Exception ex){
+            throw new RuntimeException("transform file into bin String 出错",ex);
+        }
+    }
+
+
+	/**
+	 * 二进制字符串转文件
+	 * @param bin
+	 * @param fileName
+	 * @param parentPath
+	 * @return
+	 */
+    public static File binToFile(String bin,String fileName,String parentPath){
+        try {
+            File fout = new File(parentPath,fileName);
+            fout.createNewFile();
+            byte[] bytes1 = bin.getBytes("ISO-8859-1");
+            FileCopyUtils.copy(bytes1,fout);
+
+            //FileOutputStream outs = new FileOutputStream(fout);
+            //outs.write(bytes1);
+            //outs.flush();
+            //outs.close();
+
+            return fout;
+        }catch (Exception ex){
+            throw new RuntimeException("transform bin into File 出错",ex);
+        }
+    }
+
+    /**
+     * 文件转为二进制数组
+     * 等价于fileToBin
+     * @param file
+     * @return
+     */
+    public static byte[] getFileToByte(File file) {
+        byte[] by = new byte[(int) file.length()];
+        try {
+            InputStream is = new FileInputStream(file);
+            ByteArrayOutputStream bytestream = new ByteArrayOutputStream();
+            byte[] bb = new byte[2048];
+            int ch;
+            ch = is.read(bb);
+            while (ch != -1) {
+                bytestream.write(bb, 0, ch);
+                ch = is.read(bb);
+            }
+            by = bytestream.toByteArray();
+        } catch (Exception ex) {
+            throw new RuntimeException("transform file into bin Array 出错",ex);
+        }
+        return by;
+    }
+    
+    private static byte[] byteMergerAll(byte[]... values) {
+        int length_byte = 0;
+            for (int i = 0; i < values.length; i++) {
+                length_byte += values[i].length;
+            }
+            byte[] all_byte = new byte[length_byte];
+            int countLength = 0;
+            for (int i = 0; i < values.length; i++) {
+                byte[] b = values[i];
+                System.arraycopy(b, 0, all_byte, countLength, b.length);
+                countLength += b.length;
+            }
+            return all_byte;
+        }
+    
+    public static void main(String[] args){
+        File file = new File("D://新建文本文档.txt");
+//        String fileName = file.getName();
+//        binToFile(fileToBinStr(file),fileName,"D://测试byte");
+//        getFileToByte(file);
+        
+        byte[] fileByte = fileToBinArray(file);
+        System.out.println(fileByte.length);
+        int fileLength = fileByte.length;
+        int length = 16;
+        int packNum = fileLength/length;
+        System.out.println(packNum);
+        
+        Map<String,Object> fileMap = new LinkedHashMap<String,Object>(packNum);
+        for (int i = 0; i <= (packNum); i++) {
+        	int blen = (int) ((i + 1) * length > fileLength ? fileLength : (i + 1) * length);
+			byte[] b = Arrays.copyOfRange(fileByte, length * i, blen);
+			fileMap.put(String.valueOf(i), b);
+		}
+        
+        byte[] all_byte = new byte[fileLength];
+        Iterator<Entry<String, Object>> it = fileMap.entrySet().iterator();
+		int countLength = 0;
+		while (it.hasNext()) {
+			Entry<String, Object> entry = it.next();
+			byte[] b = (byte[]) entry.getValue();
+			System.arraycopy(b, 0, all_byte, countLength, b.length);
+			countLength += b.length;
+			System.out.println("countLength: "+countLength);
+		}
+		CommFunc.byte2image(all_byte, "D://dbr.txt");
+    }
 }
