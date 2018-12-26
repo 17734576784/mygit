5f00368b4997198cc432078fd6edf3581b0d8348
17734576784 <17734576784@163.com>
2018-12-26 18:50:46 +0800

    d

--- httpstest/src/httpUtil/HttpsClientUtil.java
+++ httpstest/src/httpUtil/HttpsClientUtil.java
@@ -53,6 +53,8 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.message.BasicNameValuePair;
 
+import utils.Constant;
+
 /**
  * @ClassName: HTTPSClientUtil
  * @Description: TODO(这里用一句话描述这个类的作用)
