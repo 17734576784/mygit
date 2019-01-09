6093979e39e1e21df0bade501432fcf7d1a4b138
Administrator <Administrator@10_163_100_16>
2019-01-09 11:05:37 +0800

    测试时间转换

--- NbPlatform/src/test/java/com/nb/NbPlatformApplicationTests.java
+++ NbPlatform/src/test/java/com/nb/NbPlatformApplicationTests.java
@@ -1,5 +1,8 @@
 package com.nb;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +14,13 @@ public class NbPlatformApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
+		long time = 1546939347401L; 
+		
+ 		Calendar calendar = Calendar.getInstance();
+		calendar.setTimeInMillis(time);
+		SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-DD HH:mm:ss");
+		System.out.println(sdf.format(calendar.getTime()));
 	}
 
 }
